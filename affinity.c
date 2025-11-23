03111a3f2c433faf8eaa492b3e7c1d1fbaeda5d9
George5683 <newtech20111@hotmail.com>
2025-11-23 12:27:59 -0500

    Added comments with explanations

--- affinity.c
+++ affinity.c
@@ -17,9 +17,11 @@ void* thread_function(){
 
 int main(){
     printf("CPU Affinity demo\n");
+    // creating the variables for the threads
     pthread_t thread1; 
     pthread_t thread2;
 
+    // create variable for cpu sets, initialize them to be an empty set, and add the desired cpus to each set
     cpu_set_t  cpusA;
     CPU_ZERO(&cpusA);
     CPU_SET(10, &cpusA);
@@ -30,7 +32,7 @@ int main(){
     CPU_SET(0, &cpusB);
     CPU_SET(1, &cpusB);
 
-
+    // variables for timing (using clock() needs to use clock_t type while using time() needs to use time_t type)
     time_t start_t1,end_t1, start_t2, end_t2;
     
     
@@ -38,32 +40,34 @@ int main(){
     int id1 = 1;
     int id2 = 2;
 
+    // create the thread, assingle the function to be executed and its argument
     start_t1=clock();
     if (pthread_create(&thread1, NULL, thread_function, (void *)&id1) != 0) {
         perror("Failed to create thread 1");
         return -1;
     }
 
-    
+    // set the affinity of the thread to the desired cpu set
     pthread_setaffinity_np(thread1, sizeof(cpusA), &cpusA);
 
-
+    // wait for the thread to finish and store the end time
     if (pthread_join(thread1, NULL) != 0) {
         perror("Failed to join thread 1");
         return -1;
     }
     end_t1 = clock();
 
-
+    // create the second thread, assign the function to be executed and its argument
     start_t2=clock();
     if (pthread_create(&thread2, NULL, thread_function, (void *)&id2) != 0) {
         perror("Failed to create thread 2");
         return -1;
     }
 
+    // set the affinity of the thread to the desired cpu set
      pthread_setaffinity_np(thread2, sizeof(cpusA), &cpusA);
 
-
+    // wait for the thread to finish and store the end time
      if (pthread_join(thread2, NULL) != 0) {
         perror("Failed to join thread 1");
         return -1;
@@ -74,6 +78,7 @@ int main(){
     end_t2=clock();
     int t1 =(end_t1-start_t1);
     int t2 =(end_t2-start_t2);
+    
     //time_t elapsed = time(timer);
     printf("Thread 1 has finished %d cycles.\n", t1);
     printf("Thread 2 has finished %d cycles.\n", t2);
